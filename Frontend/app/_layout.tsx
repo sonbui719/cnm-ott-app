7fdd1cd848eb60d16bdafebce99e8dadcbcb360d
Buitrungkien27 <buitrungkien2711104@gmail.com>
2026-04-10 23:18:21 +0700

    fe dang ky

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: "#050505" },
+      }}
+    />
   );
 }
